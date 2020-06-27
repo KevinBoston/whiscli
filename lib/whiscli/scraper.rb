514dd7409118775c8607df8de363afc691d3f6ef
Kevin Boston <kbos22@gmail.com>
2020-06-27 16:26:31 +0000

    possible price fix

--- lib/whiscli/scraper.rb
+++ lib/whiscli/scraper.rb
@@ -1,7 +1,32 @@
 class Whiscli::Scraper
   require 'open-uri'
 
- # def find_categories
+ 
+  def initialize
+    
+  end
+   
+  def scrape_whisky(whisky_obj)
+    page = Nokogiri::HTML(open(whisky_obj.link))
+    #binding.pry
+    #whisky_obj.price = page.css('.price').text.strip
+    whisky_obj.description = page.css('.tab-content p').text
+    whisky_obj
+  end
+  def find_whisky(url, selection)
+    doc = Nokogiri::HTML(open(url))
+    frames = doc.css('.card-title a')
+    bottom = doc.css('.card-text span.price').text.split.uniq!
+    binding.pry
+    frames.each do |whisky_frame|
+      new_whisk = Whiscli::Whisky.new
+      new_whisk.name = whisky_frame.text
+      new_whisk.link = whisky_frame['href']
+      new_whisk.category = selection
+      new_whisk.price = bottom[1]
+    end
+  end
+  # def find_categories
     # #faceted-search-container > div > ul > li.active-category.collapsable > ul
     #navigate to spirits menu
     #compare headings to ensure they contain 'whisk'
@@ -10,27 +35,4 @@ class Whiscli::Scraper
 
    # doc
   #end
-  def initialize
-    
-  end
-   
-  def scrape_whisky(whisky_obj)
-    page = Nokogiri::HTML(open(whisky_obj.link))
-    binding.pry
-    whisky_obj.price = page.css('.price').text.strip
-    whisky_obj.description = page.css('.tab-content p').text
-    whisky_obj
-  end
-  def find_whisky(url, selection)
-    doc = Nokogiri::HTML(open(url))
-    frames = doc.css('.card-title a')
-    #binding.pry
-    frames.each do |whisky_frame|
-      new_whisk = Whiscli::Whisky.new
-      new_whisk.name = whisky_frame.text
-      new_whisk.link = whisky_frame['href']
-      new_whisk.category = selection
-      #new_whisk.price = 
-    end
-  end
 end
